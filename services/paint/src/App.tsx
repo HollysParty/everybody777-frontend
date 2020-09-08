eca32fed9d0beca9cab65ec25b3bf796cdb8a568
mysticPrg <mysticPrg@gmail.com>
2020-09-08 17:44:26 +0900

    sketch 목록 가져오는 컴포넌트 추가

--- services/paint/src/App.tsx
+++ services/paint/src/App.tsx
@@ -1,60 +1,18 @@
 import React from 'react';
-import ApolloClient, { gql } from 'apollo-boost';
-import { ApolloProvider, Query, QueryResult } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import SketchList from './SketchList';
+import { END_POINT_URL, END_POINT_PATH } from './constants/graphql';
 
 const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com'
+  uri: `${END_POINT_URL}${END_POINT_PATH}`
 });
-
-const GET_CONTINENTS = gql`
-  query {
-    continents {
-      code
-      name
-    }
-  }
-`;
-
-type Continents = {
-  continents: {
-    code: string;
-    name: string;
-  }[];
-};
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <p>This is paint client</p>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Query query={GET_CONTINENTS}>
-          {(result: QueryResult<Continents, Record<string, any>>) => {
-            const { loading, error, data } = result;
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error!(</p>;
-            return (
-              <ul>
-                {data &&
-                  data.continents.map(({ code, name }) => (
-                    <li key={code}>{name}</li>
-                  ))}
-              </ul>
-            );
-          }}
-        </Query>
+      <div>
+        <SketchList />
       </div>
     </ApolloProvider>
   );
---
+++ services/paint/src/SketchList.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useApolloClient, useQuery, QueryResult } from 'react-apollo';
+import { END_POINT_URL } from './constants/graphql';
+
+const GET_SKETCHES = gql`
+  query {
+    sketches {
+      id
+      imageUrl
+    }
+  }
+`;
+
+interface Sketch {
+  id: string;
+  imageUrl: string;
+}
+
+interface GetSketchesResult {
+  sketches: Sketch[];
+}
+
+export default function SketchList() {
+  const { data, error, loading } = useQuery<GetSketchesResult>(GET_SKETCHES);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!data || error) {
+    return <p>Error!(</p>;
+  }
+
+  const { sketches } = data;
+
+  return (
+    <ul>
+      {sketches.map(({ id, imageUrl }) => (
+        <li key={id}>
+          <div>
+            <p>id: {id}</p>
+            <p>
+              <img src={`${END_POINT_URL}${imageUrl}`} alt={id} />
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
