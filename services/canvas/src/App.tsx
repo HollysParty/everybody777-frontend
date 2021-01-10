94b00f2b2c2b1270108465b1406b4d7d56f1d808
sunny kyung <shkyung@lunit.io>
2021-01-11 07:56:46 +0900

    implement draft CanvasProvider and wrapped the App component

--- services/canvas/src/App.tsx
+++ services/canvas/src/App.tsx
@@ -7,24 +7,27 @@ import {
 } from 'react-router-dom';
 import Entry from './components/Entry';
 import Drawing from './components/Drawing';
+import { CanvasProvider } from './context/CanvasProvider';
 
 const baseName = '/everybody777-frontend/canvas';
 
 function App() {
   return (
-    <Router basename={baseName}>
-      <Switch>
-        <Route
-          exact
-          path="/paint/:id"
-          render={({ match }) => <Drawing id={match.params.id} />}
-        />
-        <Route exact path="/" component={Entry} />
-        <Route>
-          <Redirect to="/" />
-        </Route>
-      </Switch>
-    </Router>
+    <CanvasProvider>
+      <Router basename={baseName}>
+        <Switch>
+          <Route
+            exact
+            path="/paint/:id"
+            render={({ match }) => <Drawing id={match.params.id} />}
+          />
+          <Route exact path="/" component={Entry} />
+          <Route>
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </CanvasProvider>
   );
 }
 export default App;
